ab38ba7e56e07cf0fa2016c20664ee7f140b8493
Ben Moon <guiltydolphin@gmail.com>
2016-08-21 18:20:32 +0100

    Move main hook functions into separate file

--- hook-core.el
+++ hook-core.el
@@ -1,4 +1,4 @@
-;;; hook.el --- Utilities for generating hooks.
+;;; hook-core.el --- Utilities for generating hooks.
 ;;;
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -70,5 +70,5 @@ If PRED evaluates to non-NIL, then run each function in FNS."
 
 (add-hook 'post-command-hook 'hook--check-monitored)
 
-(provide 'hook)
-;;; hook.el ends here
+(provide 'hook-core)
+;;; hook-core.el ends here
