78b34ecf2c00a274cafcda562afec9ba6fb54c2d
Ben Moon <guiltydolphin@gmail.com>
2016-08-22 17:04:56 +0100

    Add package comment header

--- hook.el
+++ hook.el
@@ -1,10 +1,18 @@
 ;;; hook.el --- Utilities for generating hooks.
-;;;
+
+;; Copyright (C) 2016 Ben Moon
+;; Author: Ben Moon <guiltydolphin@gmail.com>
+;; URL: https://github.com/guiltydolphin/hook
+;; Git-Repository: git://github.com/guiltydolphin/hook.git
+;; Created: 2016-08-17
+;; Version: 0.1.0
+;; Keywords: hook utility
+;; Package-Requires: ((dash "2.13.0"))
+
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-;;
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
